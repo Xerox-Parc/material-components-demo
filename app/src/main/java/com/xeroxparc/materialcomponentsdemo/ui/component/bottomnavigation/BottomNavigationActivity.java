dff74e49a72859d19c2fdce21203d93e321f21d3
Ivan <Ivan-Palm@users.noreply.github.com>
2020-05-31 17:08:02 +0200

    Update #10
    - Fix for implement new functionality
    - Added toast when swapping pages, added new icones
    - Only affects highlight class

--- app/src/main/java/com/xeroxparc/materialcomponentsdemo/ui/component/bottomnavigation/BottomNavigationActivity.java
+++ app/src/main/java/com/xeroxparc/materialcomponentsdemo/ui/component/bottomnavigation/BottomNavigationActivity.java
@@ -3,6 +3,7 @@ package com.xeroxparc.materialcomponentsdemo.ui.component.bottomnavigation;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,10 +28,26 @@ public class BottomNavigationActivity extends AppCompatActivity {
             binding.appBarContainer.toolbar.setTitle(R.string.bottom_navigation_title);
             binding.appBarContainer.imageViewBanner.setImageResource(R.drawable.banner_bottomnavigation);
             inflateSpanTextViewContent(binding, activity);
-            //TODO
+            binding.bottomNavigation.setOnNavigationItemSelectedListener(item -> {
+                switch (item.getItemId()) {
+                    case R.id.page_1:
+                        Toast.makeText(BottomNavigationActivity.this, "Page favorite", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.page_2:
+                        Toast.makeText(BottomNavigationActivity.this, "Page music", Toast.LENGTH_SHORT).show();
+                        break ;
+                    case R.id.page_3:
+                        Toast.makeText(BottomNavigationActivity.this, "Page places", Toast.LENGTH_SHORT).show();
+                        break ;
+                    case R.id.page_4:
+                        Toast.makeText(BottomNavigationActivity.this, "Page news", Toast.LENGTH_SHORT).show();
+                        break ;
+                }
+                return true;
+            });
         }
-        View getRoot() {
-            return binding.getRoot();
+        View getRoot () {
+                return binding.getRoot();
+            }
         }
-    }
 }

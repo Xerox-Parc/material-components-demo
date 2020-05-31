232d0522ca2684a08c70a753e54378add165a21d
Marco Purificato <purificato_marco@libero.it>
2020-05-31 15:25:34 +0200

    Add #12 demo structure
    - Implement demo structure
    - Add layout, activity, string resource
    - Only affects highlight class

---
+++ app/src/main/java/com/xeroxparc/materialcomponentsdemo/ui/component/fab/FabActivity.java
@@ -0,0 +1,48 @@
+package com.xeroxparc.materialcomponentsdemo.ui.component.fab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.xeroxparc.materialcomponentsdemo.R;
+import com.xeroxparc.materialcomponentsdemo.databinding.ActivityFabBinding;
+
+import static com.xeroxparc.materialcomponentsdemo.utils.Utils.inflateSpanTextViewContent;
+
+public class FabActivity extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Holder holder = new Holder(this);
+        setContentView(holder.getRoot());
+    }
+
+
+    class Holder {
+
+
+        private ActivityFabBinding binding;
+
+
+        Holder(Activity activity) {
+            binding = ActivityFabBinding.inflate(getLayoutInflater());
+            binding.appBarContainer.toolbar.setTitle(R.string.fab_title);
+            binding.appBarContainer.imageViewBanner.setImageResource(R.drawable.banner_fab);
+
+
+            inflateSpanTextViewContent(binding, activity);
+
+
+            //TODO
+        }
+
+
+        View getRoot() {
+            return binding.getRoot();
+        }
+    }
+}

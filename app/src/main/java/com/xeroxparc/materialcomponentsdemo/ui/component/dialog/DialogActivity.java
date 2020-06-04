74fd7232d69b54c2bb740ff25977f553a6787e16
Marco Purificato <purificato_marco@libero.it>
2020-06-04 20:14:36 +0200

    Add #16  Demo
    - Add Dialogs sample
    - String, Layout and Java Class
    - Only affects highlight class

--- app/src/main/java/com/xeroxparc/materialcomponentsdemo/ui/component/dialog/DialogActivity.java
+++ app/src/main/java/com/xeroxparc/materialcomponentsdemo/ui/component/dialog/DialogActivity.java
@@ -3,9 +3,12 @@ package com.xeroxparc.materialcomponentsdemo.ui.component.dialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.xeroxparc.materialcomponentsdemo.R;
 import com.xeroxparc.materialcomponentsdemo.databinding.ActivityDialogBinding;
 
@@ -36,8 +39,51 @@ public class DialogActivity extends AppCompatActivity {
 
             inflateSpanTextViewContent(binding, activity);
 
+            binding.materialButtonAlertDialog.setOnClickListener(l -> {
+                new MaterialAlertDialogBuilder(DialogActivity.this)
+                        .setMessage("Discard Draft?")
+                        .setPositiveButton("DISCARD", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) { }
+                        })
+                        .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) { }
+                        })
+                        .show();
+            });
 
-            //TODO
+            binding.materialButtonSimpleDialog.setOnClickListener(l -> {
+                String[] singleChoiceItems = getResources().getStringArray(R.array.simple_dialog_array);
+                int itemSelected = 0;
+                new MaterialAlertDialogBuilder(DialogActivity.this)
+                        .setTitle("View options")
+                        .setSingleChoiceItems(singleChoiceItems, itemSelected, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int selectedIndex) {
+
+                            }
+                        })
+                        .setPositiveButton("ACCEPT", null)
+                        .show();
+            });
+
+            binding.materialButtonConfirmationDialog.setOnClickListener(l -> {
+                String[] singleChoiceItems = getResources().getStringArray(R.array.confirmation_dialog_array);
+                int itemSelected = 0;
+                new MaterialAlertDialogBuilder(DialogActivity.this)
+                        .setTitle("Phone Ringtone")
+                        .setSingleChoiceItems(singleChoiceItems, itemSelected, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int selectedIndex) {
+
+                            }
+                        })
+                        .setPositiveButton("OK", null)
+                        .setNegativeButton("CANCEL", null)
+                        .show();
+            });
+
         }
 
 

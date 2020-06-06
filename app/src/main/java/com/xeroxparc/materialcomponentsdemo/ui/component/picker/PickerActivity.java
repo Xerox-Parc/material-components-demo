936901ae8e5be8afa374a8aad8726f90a1504111
DaniloDamico <45599533+DaniloDamico@users.noreply.github.com>
2020-06-06 20:59:18 +0200

    feature/picker
    Adds picker component in both datePicker and dateRangePicker form

--- app/src/main/java/com/xeroxparc/materialcomponentsdemo/ui/component/picker/PickerActivity.java
+++ app/src/main/java/com/xeroxparc/materialcomponentsdemo/ui/component/picker/PickerActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import android.view.View;
+import android.widget.Toast;
 
 import com.xeroxparc.materialcomponentsdemo.R;
 
@@ -14,9 +15,6 @@ import static com.xeroxparc.materialcomponentsdemo.utils.Utils.inflateSpanTextVi
 import com.google.android.material.datepicker.MaterialDatePicker;
 
 import com.xeroxparc.materialcomponentsdemo.databinding.ActivityPickerBinding;
-
-import java.util.Calendar;
-import java.util.TimeZone;
 
 public class PickerActivity extends AppCompatActivity{
 
@@ -47,19 +45,27 @@ public class PickerActivity extends AppCompatActivity{
 
         @Override
         public void onClick(View v) {
-            Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Europe/Rome"));
-            calendar.clear();
             if (v.getId() == R.id.btn_mobile_date_range_picker_button) {
                 MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.dateRangePicker();
                 builder.setTitleText(R.string.date_range_picker);
-                final MaterialDatePicker materialDatePicker = builder.build();
-                materialDatePicker.show(getSupportFragmentManager(), "DATE_PICKER");
+                try {
+                    final MaterialDatePicker materialDatePicker = builder.build();
+                    materialDatePicker.show(getSupportFragmentManager(), "DATE_PICKER");
+                } catch (IllegalArgumentException e){
+                    Toast.makeText(getApplicationContext(), "The launch of the selected picker has failed", Toast.LENGTH_LONG).show();
+                }
             }
             if (v.getId() == R.id.btn_mobile_calendar_picker){
                 MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.datePicker();
                 builder.setTitleText(R.string.calendar_picker);
-                final MaterialDatePicker materialDatePicker = builder.build();
-                materialDatePicker.show(getSupportFragmentManager(), "DATE_PICKER");
+                builder.setSelection(MaterialDatePicker.todayInUtcMilliseconds());
+                try {
+                    final MaterialDatePicker materialDatePicker = builder.build();
+                    materialDatePicker.show(getSupportFragmentManager(), "DATE_PICKER");
+                } catch (IllegalArgumentException e){
+                    Toast.makeText(getApplicationContext(), "This is my Toast message!",
+                            Toast.LENGTH_LONG).show();
+                }
             }
         }
     }

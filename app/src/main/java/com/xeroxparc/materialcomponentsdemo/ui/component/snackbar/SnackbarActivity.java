cb548d1be1b2646fa8935d874605cb244a30f844
Alessio1100 <alex.torroni00@gmail.com>
2020-06-06 11:40:18 +0200

    implement #28
    - Implement demo structure
    - Add layout, activity, string resource
    - Only affects highlight class

---
+++ app/src/main/java/com/xeroxparc/materialcomponentsdemo/ui/component/snackbar/SnackbarActivity.java
@@ -0,0 +1,50 @@
+package com.xeroxparc.materialcomponentsdemo.ui.component.snackbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.google.android.material.snackbar.Snackbar;
+import com.xeroxparc.materialcomponentsdemo.R;
+import com.xeroxparc.materialcomponentsdemo.databinding.ActivitySnackbarBinding;
+
+import static com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_LONG;
+import static com.xeroxparc.materialcomponentsdemo.utils.Utils.inflateSpanTextViewContent;
+
+public class SnackbarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Holder holder = new Holder(this);
+        setContentView(holder.getRoot());
+    }
+
+    class Holder {
+
+        private ActivitySnackbarBinding binding;
+
+        Holder(Activity activity) {
+            binding = ActivitySnackbarBinding.inflate(getLayoutInflater());
+            binding.appBarContainer.toolbar.setTitle(R.string.snackbar_title);
+            binding.appBarContainer.imageViewBanner.setImageResource(R.drawable.banner_snackbar);
+
+            inflateSpanTextViewContent(binding, activity);
+            binding.snackbarButton.setOnClickListener(l ->
+                    Snackbar.make(binding.coordinator,
+                            "THIS IS A SNACKBAR",
+                            LENGTH_LONG).show());
+            binding.snackbarButtonUndo.setOnClickListener(l ->
+                    Snackbar.make(binding.coordinator,
+                            "THIS IS A SNACKBAR",
+                            LENGTH_LONG).setAction("UNDO", v -> Log.e("UNDO","Done")).show());
+        }
+        View getRoot() {
+            return binding.getRoot();
+        }
+    }
+}

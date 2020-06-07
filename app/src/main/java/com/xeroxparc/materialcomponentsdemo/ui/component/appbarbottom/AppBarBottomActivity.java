505063c3fd41d44b5166909989de7b40b9cc0883
Ivan <Ivan-Palm@users.noreply.github.com>
2020-06-08 00:44:17 +0200

    Update #7
    - Fix of graphic elements of the bottom bar app
    - Update strings value, delete navigationIcon, delete oblsolete vector image
    - Only affects highlight class

--- app/src/main/java/com/xeroxparc/materialcomponentsdemo/ui/component/appbarbottom/AppBarBottomActivity.java
+++ app/src/main/java/com/xeroxparc/materialcomponentsdemo/ui/component/appbarbottom/AppBarBottomActivity.java
@@ -1,3 +1,4 @@
+
 package com.xeroxparc.materialcomponentsdemo.ui.component.appbarbottom;
 
 import android.app.Activity;
@@ -14,37 +15,35 @@ import static com.xeroxparc.materialcomponentsdemo.utils.Utils.inflateSpanTextVi
 
 public class AppBarBottomActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
 
-        super.onCreate(savedInstanceState);
-        Holder holder = new Holder(this);
-        setContentView(holder.getRoot());
-    }
-    class Holder {
-        private ActivityAppBarBottomBinding binding;
-        Holder(Activity activity) {
-            binding = ActivityAppBarBottomBinding.inflate(getLayoutInflater());
-            binding.appBarContainer.toolbar.setTitle(R.string.app_bar_bottom_title);
-            binding.appBarContainer.imageViewBanner.setImageResource(R.drawable.banner_appbarbottom);
-            inflateSpanTextViewContent(binding, activity);
-            binding.bottomAppBar.setNavigationOnClickListener(v -> Toast.makeText(activity, R.string.app_bar_bottom_press_more, Toast.LENGTH_LONG).show());
-            binding.bottomAppBar.setOnMenuItemClickListener(v -> {
-                if(v.getItemId()==R.id.more) {
-                    Toast.makeText(activity, R.string.app_bar_bottom_press_more_information, Toast.LENGTH_LONG).show();
-                }
-                else if(v.getItemId()==R.id.search){
-                    Toast.makeText(activity, R.string.app_bar_bottom_press_search, Toast.LENGTH_LONG).show();
-                }
-                return false;
-            });
-            binding.fab.setOnClickListener(l->{
-                Toast.makeText(activity, R.string.app_bar_bottom_press_floating, Toast.LENGTH_LONG).show();
-            });
-        }
-        View getRoot() {
-            return binding.getRoot();
-        }
-    }
+super.onCreate(savedInstanceState);
+Holder holder = new Holder(this);
+setContentView(holder.getRoot());
 }
-
+class Holder {
+private ActivityAppBarBottomBinding binding;
+Holder(Activity activity) {
+    binding = ActivityAppBarBottomBinding.inflate(getLayoutInflater());
+    binding.appBarContainer.toolbar.setTitle(R.string.app_bar_bottom_title);
+    binding.appBarContainer.imageViewBanner.setImageResource(R.drawable.banner_appbarbottom);
+    inflateSpanTextViewContent(binding, activity);
+    binding.bottomAppBar.setOnMenuItemClickListener(v -> {
+        if(v.getItemId()==R.id.more) {
+            Toast.makeText(activity, R.string.app_bar_bottom_press_more_information, Toast.LENGTH_LONG).show();
+        }
+        else if(v.getItemId()==R.id.search){
+            Toast.makeText(activity, R.string.app_bar_bottom_press_star, Toast.LENGTH_LONG).show();
+        }
+        return false;
+    });
+    binding.fab.setOnClickListener(l->{
+        Toast.makeText(activity, R.string.app_bar_bottom_press_floating, Toast.LENGTH_LONG).show();
+    });
+}
+View getRoot() {
+    return binding.getRoot();
+}
+}
+}

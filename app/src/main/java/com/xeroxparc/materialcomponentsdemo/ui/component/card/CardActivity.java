0b56d23f4be64ed7370b94ce9d784a723c2c4c58
Marco Purificato <purificato_marco@libero.it>
2020-06-07 18:06:34 +0200

    Fix #13  demo
    - Fix from review
    - String, layout and java class
    - Only affects highlight class

--- app/src/main/java/com/xeroxparc/materialcomponentsdemo/ui/component/card/CardActivity.java
+++ app/src/main/java/com/xeroxparc/materialcomponentsdemo/ui/component/card/CardActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.xeroxparc.materialcomponentsdemo.R;
 import com.xeroxparc.materialcomponentsdemo.databinding.ActivityCardBinding;
@@ -30,14 +31,14 @@ public class CardActivity extends AppCompatActivity {
 
         Holder(Activity activity) {
             binding = ActivityCardBinding.inflate(getLayoutInflater());
-            binding.appBarContainer.toolbar.setTitle(R.string.card_title);
+            binding.appBarContainer.toolbar.setTitle(getString(R.string.card_title));
             binding.appBarContainer.imageViewBanner.setImageResource(R.drawable.banner_card);
 
 
             inflateSpanTextViewContent(binding, activity);
 
-
-            //TODO
+            this.binding.cardSampleButton1.setOnClickListener(l -> { Toast.makeText(CardActivity.this, getString(R.string.card_sample_clicked_button1), Toast.LENGTH_LONG).show(); });
+            this.binding.cardSampleButton2.setOnClickListener(l -> { Toast.makeText(CardActivity.this, getString(R.string.card_sample_clicked_button2), Toast.LENGTH_LONG).show(); });
         }
 
 

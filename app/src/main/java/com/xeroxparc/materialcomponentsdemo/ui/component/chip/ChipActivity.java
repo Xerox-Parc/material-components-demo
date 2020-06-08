84370b9b4717b9fbd443352a4be2b8c925c71b5e
Marco Purificato <purificato_marco@libero.it>
2020-06-08 16:52:27 +0200

    Fix #14 demo
    - Demo improvements
    - String, layout and java class
    - Only affects highlight class

--- app/src/main/java/com/xeroxparc/materialcomponentsdemo/ui/component/chip/ChipActivity.java
+++ app/src/main/java/com/xeroxparc/materialcomponentsdemo/ui/component/chip/ChipActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.xeroxparc.materialcomponentsdemo.R;
 import com.xeroxparc.materialcomponentsdemo.databinding.ActivityChipBinding;
@@ -36,8 +37,19 @@ public class ChipActivity extends AppCompatActivity {
 
             inflateSpanTextViewContent(binding, activity);
 
+            this.binding.chipInput1.setOnClickListener(l -> { Toast.makeText(ChipActivity.this, getString(R.string.chip_input1_toast), Toast.LENGTH_LONG).show(); });
+            this.binding.chipInput2.setOnClickListener(l -> { Toast.makeText(ChipActivity.this, getString(R.string.chip_input2_toast), Toast.LENGTH_LONG).show(); });
 
-            //TODO
+            this.binding.chipFilter1.setOnClickListener(l -> { Toast.makeText(ChipActivity.this, getString(R.string.chip_filter1_toast), Toast.LENGTH_LONG).show(); });
+            this.binding.chipFilter2.setOnClickListener(l -> { Toast.makeText(ChipActivity.this, getString(R.string.chip_filter2_toast), Toast.LENGTH_LONG).show(); });
+            this.binding.chipFilter3.setOnClickListener(l -> { Toast.makeText(ChipActivity.this, getString(R.string.chip_filter3_toast), Toast.LENGTH_LONG).show(); });
+
+            this.binding.chipAction1.setOnClickListener(l -> { Toast.makeText(ChipActivity.this, getString(R.string.chip_action1_toast), Toast.LENGTH_LONG).show(); });
+            this.binding.chipAction2.setOnClickListener(l -> { Toast.makeText(ChipActivity.this, getString(R.string.chip_action2_toast), Toast.LENGTH_LONG).show(); });
+
+            this.binding.chipChoice1.setOnClickListener(l -> { Toast.makeText(ChipActivity.this, getString(R.string.chip_choice1_toast), Toast.LENGTH_LONG).show(); });
+            this.binding.chipChoice2.setOnClickListener(l -> { Toast.makeText(ChipActivity.this, getString(R.string.chip_choice2_toast), Toast.LENGTH_LONG).show(); });
+            this.binding.chipChoice3.setOnClickListener(l -> { Toast.makeText(ChipActivity.this, getString(R.string.chip_choice3_toast), Toast.LENGTH_LONG).show(); });
         }
 
 

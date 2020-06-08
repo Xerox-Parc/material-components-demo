07269f952df8caefb515990d6058a8062804384c
Ivan <Ivan-Palm@users.noreply.github.com>
2020-06-08 19:55:00 +0200

    Update #9
    - Fix interface
    - Add strings value, add image descriptor
    - Only effect hightlight class

--- app/src/main/java/com/xeroxparc/materialcomponentsdemo/ui/component/banner/BannerActivity.java
+++ app/src/main/java/com/xeroxparc/materialcomponentsdemo/ui/component/banner/BannerActivity.java
@@ -1,13 +1,11 @@
 package com.xeroxparc.materialcomponentsdemo.ui.component.banner;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.MobileAds;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.xeroxparc.materialcomponentsdemo.R;
 import com.xeroxparc.materialcomponentsdemo.databinding.ActivityBannerBinding;
 
@@ -25,13 +23,11 @@ public class BannerActivity extends AppCompatActivity {
     class Holder {
         private ActivityBannerBinding binding;
         Holder(Activity activity) {
-            binding.appBarContainer.toolbar.setTitle(R.string.banner_title);
             binding = ActivityBannerBinding.inflate(getLayoutInflater());
+            binding.appBarContainer.toolbar.setTitle(R.string.banner_title);
             binding.appBarContainer.imageViewBanner.setImageResource(R.drawable.banner_banner);
             inflateSpanTextViewContent(binding, activity);
-            //TODO
         }
-
         View getRoot() {
             return binding.getRoot();
         }

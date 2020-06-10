306441c871c8c3d21b211c307a226a7e21d48642
DaniloDamico <45599533+DaniloDamico@users.noreply.github.com>
2020-06-10 02:32:49 +0200

    Drawer destinations added

--- app/src/main/java/com/xeroxparc/materialcomponentsdemo/ui/component/navigationdrawer/NavigationDrawerActivity.java
+++ app/src/main/java/com/xeroxparc/materialcomponentsdemo/ui/component/navigationdrawer/NavigationDrawerActivity.java
@@ -1,23 +1,61 @@
 package com.xeroxparc.materialcomponentsdemo.ui.component.navigationdrawer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.Navigation;
+
+import com.google.android.material.navigation.NavigationView;
 import com.xeroxparc.materialcomponentsdemo.R;
 import com.xeroxparc.materialcomponentsdemo.databinding.ActivityNavigationDrawerBinding;
 
 import static com.xeroxparc.materialcomponentsdemo.utils.Utils.inflateSpanTextViewContent;
 
-public class NavigationDrawerActivity extends AppCompatActivity {
+public class NavigationDrawerActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Holder holder = new Holder(this);
         setContentView(holder.getRoot());
+        init();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.navigation_home:{
+                Navigation.findNavController(this, R.id.navigation_host_fragment).navigate(R.id.navigation_home);
+                break;
+            }
+            case R.id.navigation_standard_drawer:{
+                Navigation.findNavController(this, R.id.navigation_host_fragment).navigate(R.id.navigation_standard_drawer);
+                break;
+            }
+            case R.id.navigation_bottom_drawer:{
+                Navigation.findNavController(this, R.id.navigation_host_fragment).navigate(R.id.navigation_bottom_drawer);
+                break;
+            }
+        }
+
+        item.setChecked(true);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    private void init(){
+        NavigationView navigationView = findViewById(R.id.navigation_drawer_navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     class Holder {

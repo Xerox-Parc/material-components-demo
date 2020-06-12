7d583671136c079b8d1e15710d030ce643595fe3
DaniloDamico <45599533+DaniloDamico@users.noreply.github.com>
2020-06-12 18:09:52 +0200

    Suggested changes implemented

--- app/src/main/java/com/xeroxparc/materialcomponentsdemo/ui/component/navigationdrawer/NavigationDrawerActivity.java
+++ app/src/main/java/com/xeroxparc/materialcomponentsdemo/ui/component/navigationdrawer/NavigationDrawerActivity.java
@@ -2,60 +2,28 @@ package com.xeroxparc.materialcomponentsdemo.ui.component.navigationdrawer;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
-import com.google.android.material.navigation.NavigationView;
 import com.xeroxparc.materialcomponentsdemo.R;
 import com.xeroxparc.materialcomponentsdemo.databinding.ActivityNavigationDrawerBinding;
 
+import java.util.Objects;
+
 import static com.xeroxparc.materialcomponentsdemo.utils.Utils.inflateSpanTextViewContent;
 
-public class NavigationDrawerActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class NavigationDrawerActivity extends AppCompatActivity {
 
+    Holder holder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Holder holder = new Holder(this);
+        holder = new Holder(this);
         setContentView(holder.getRoot());
-        init();
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-        switch(item.getItemId()){
-            case R.id.navigation_home:{
-                Navigation.findNavController(this, R.id.navigation_host_fragment).navigate(R.id.navigation_home);
-                break;
-            }
-            case R.id.navigation_standard_drawer:{
-                Navigation.findNavController(this, R.id.navigation_host_fragment).navigate(R.id.navigation_standard_drawer);
-                break;
-            }
-            case R.id.navigation_bottom_drawer:{
-                Navigation.findNavController(this, R.id.navigation_host_fragment).navigate(R.id.navigation_bottom_drawer);
-                break;
-            }
-        }
-
-        item.setChecked(true);
-        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
-        drawerLayout.closeDrawer(GravityCompat.START);
-
-        return true;
-    }
-
-    private void init(){
-        NavigationView navigationView = findViewById(R.id.navigation_drawer_navigation_view);
-        navigationView.setNavigationItemSelectedListener(this);
     }
 
     class Holder {
@@ -67,10 +35,21 @@ public class NavigationDrawerActivity extends AppCompatActivity implements Navig
             binding.appBarContainer.imageViewBanner.setImageResource(R.drawable.banner_navigationdrawer);
 
             inflateSpanTextViewContent(binding, activity);
+
+            binding.navigationView.setNavigationItemSelectedListener(item -> {
+                ((NavHostFragment) (Objects.requireNonNull(getSupportFragmentManager()
+                        .findFragmentById(binding.navigationHostFragment.getId()))))
+                        .getNavController()
+                        .navigate(item.getItemId());
+                item.setChecked(true);
+                binding.drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            });
         }
 
         View getRoot() {
             return binding.getRoot();
         }
+
     }
 }

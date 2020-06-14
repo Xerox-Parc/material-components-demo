f2176761c9a7c7e1d436195e80e29f64fef37185
Ivan <Ivan-Palm@users.noreply.github.com>
2020-06-14 02:14:55 +0200

    Update #6
    - Delete component
    - Delete app bar top, add image into drawable, update strings value
    - Only affect hightlight class

--- app/src/main/java/com/xeroxparc/materialcomponentsdemo/ui/component/appbartop/AppBarTopActivity.java
+++ app/src/main/java/com/xeroxparc/materialcomponentsdemo/ui/component/appbartop/AppBarTopActivity.java
@@ -30,21 +30,6 @@ public class AppBarTopActivity extends AppCompatActivity {
             binding.appBarContainer.toolbar.setTitle(R.string.app_bar_top_title);
             binding.appBarContainer.imageViewBanner.setImageResource(R.drawable.banner_appbartop);
             inflateSpanTextViewContent(binding, activity);
-            binding.topAppBar.setOnMenuItemClickListener(l-> {
-                if(l.getItemId()==R.id.blocked){
-                    Toast.makeText(getApplicationContext(),"You have press blocked",Toast.LENGTH_SHORT).show();
-                }
-                if(l.getItemId()==R.id.notblocked){
-                    Toast.makeText(getApplicationContext(),"You have press not blocked",Toast.LENGTH_SHORT).show();
-                }
-                if(l.getItemId()==R.id.position){
-                    Toast.makeText(getApplicationContext(),"You have press position bottom",Toast.LENGTH_SHORT).show();
-                }
-                if(l.getItemId()==R.id.search){
-                    Toast.makeText(getApplicationContext(),"You have press search bottom",Toast.LENGTH_SHORT).show();
-                }
-                return false;
-            });
         }
         View getRoot() {
             return binding.getRoot();

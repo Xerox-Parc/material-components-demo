237d071771b5ad2386a7d50126af153b4b590b24
DaniloDamico <45599533+DaniloDamico@users.noreply.github.com>
2020-06-16 17:00:47 +0200

    Implemented requested changes

--- app/src/main/java/com/xeroxparc/materialcomponentsdemo/SplashScreenActivity.java
+++ app/src/main/java/com/xeroxparc/materialcomponentsdemo/SplashScreenActivity.java
@@ -9,20 +9,22 @@ import android.os.Handler;
 import com.xeroxparc.materialcomponentsdemo.ui.search.ComponentSearchActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
-    private static int SPLASH_SCREEN_DURATION = 1000;
+
+    private static final int SPLASH_SCREEN_DURATION = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        new Handler().postDelayed(new Runnable(){
-            @Override
-            public void run() {
-                Intent splashScreenIntent = new Intent(SplashScreenActivity.this, ComponentSearchActivity.class);
-                startActivity(splashScreenIntent);
-                finish();
-            }
-        }, SPLASH_SCREEN_DURATION);
+        new Handler().postDelayed(() -> {
+                    Intent splashScreenIntent = new Intent(
+                            SplashScreenActivity.this,
+                            ComponentSearchActivity.class
+                    );
+                    startActivity(splashScreenIntent);
+                    finish();
+                }, SPLASH_SCREEN_DURATION
+        );
     }
 }
